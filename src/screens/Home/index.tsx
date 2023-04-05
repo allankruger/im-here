1f4f50901d7eace6b20e2716bbd7d7dd5249c405
allankruger <allanfloreskruger@gmail.com>
2023-04-05 15:58:55 -0300

    adjusts styles in Home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -10,14 +10,17 @@ export default function Home() {
     <View style={styles.container}>
       <Text style={styles.eventName}>Event name</Text>
       <Text style={styles.eventDate}>Friday, November 4th 2022</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Atendee name"
-        placeholderTextColor={"#6b6b6b"}
-      />
-      <TouchableOpacity style={styles.button} onPress={handleAddAtendee}>
-        <Text style={styles.buttonText}>+</Text>
-      </TouchableOpacity>
+
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Atendee name"
+          placeholderTextColor={"#6b6b6b"}
+        />
+        <TouchableOpacity style={styles.button} onPress={handleAddAtendee}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
