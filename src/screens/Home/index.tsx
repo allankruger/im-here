58b251cb5f8b313184deee843f4de16286c6d3c5
allankruger <allanfloreskruger@gmail.com>
2023-04-05 15:53:07 -0300

    adds Add button to Home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,16 +1,23 @@
-import { View, Text, TextInput } from "react-native";
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
 import { styles } from "./styles";
 
 export default function Home() {
+  function handleAddAtendee() {
+    console.log("You clicked the button");
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>Event name</Text>
-      <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022</Text>
+      <Text style={styles.eventDate}>Friday, November 4th 2022</Text>
       <TextInput
         style={styles.input}
-        placeholder="Nome do participante"
+        placeholder="Atendee name"
         placeholderTextColor={"#6b6b6b"}
       />
+      <TouchableOpacity style={styles.button} onPress={handleAddAtendee}>
+        <Text style={styles.buttonText}>+</Text>
+      </TouchableOpacity>
     </View>
   );
 }
